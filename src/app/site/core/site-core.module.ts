9c5fc0d6006967afbe7b8deda7cb0de3dd67c258
Kyrillos Mamdoh <Kyrillosmamdoh10@gmail.com>
2021-06-06 16:49:09 +0200

    first commit

---
+++ src/app/site/core/site-core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SiteCoreRoutingModule } from './site-core-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    SiteCoreRoutingModule
+  ]
+})
+export class SiteCoreModule { }
---
+++ src/app/site/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SiteLayoutComponent } from './components/site-layout/site-layout.component';
+
+
+@NgModule({
+  declarations: [SiteLayoutComponent],
+  imports: [
+  CommonModule
+  ]
+})
+export class SharedModule { }
